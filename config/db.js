0bdb86f28f6da96057fcaa17fde5464e700dd840
Udit <uditde.com@gmail.com>
2023-08-12 20:51:33 +0530

    strictQuery added to mongoose

--- config/db.js
+++ config/db.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectWithDb = () => {
 	mongoose
+		.set("strictQuery", true)
 		.connect(`${process.env.DB_URL}`, {
 			useNewUrlParser: true,
 			useUnifiedTopology: true,
