9c6afa8f516fde4dcef958738f5a52704eeb9cb2
Udit <uditde.com@gmail.com>
2023-08-12 23:32:29 +0530

    db modified

--- config/db.js
+++ config/db.js
@@ -4,16 +4,12 @@ const mongoose = require("mongoose");
 const connectWithDb = () => {
 	mongoose
 		.set("strictQuery", true)
-		.connect(
-			"mongodb+srv://udit:ygvskY1JNKa0YsLS@resqwild.egpdsfl.mongodb.net/",
-			{
-				useNewUrlParser: true,
-				useUnifiedTopology: true,
-			}
-		)
+		.connect(`${process.env.DB_URL}`, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
 		.then(console.log("DB Connected!!"))
 		.catch((err) => {
-			console.log(process.env.DB_URL);
 			console.log(`DB Connection issues => \n ${err}`);
 			process.exit(1);
 		});
