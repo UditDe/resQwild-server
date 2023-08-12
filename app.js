f36eb6e31e1695780db8307a2f167570574c28b3
Udit <uditde.com@gmail.com>
2023-08-12 12:02:24 +0530

    local db connected

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+require("dotenv").config();
+const morgan = require("morgan");
+const app = express();
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+
+//morgan middleware
+app.use(morgan("tiny"));
+
+//regular middleware
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+//cookie and file middleware
+app.use(cookieParser());
+app.use(
+	fileUpload({
+		useTempFiles: true,
+		tempFileDir: "/tmp/",
+	})
+);
+
+//import all routes here
+
+//router middleware
+
+app.get("/", (req, res) => {
+	res.status(200).json({
+		success: true,
+		message: "running",
+	});
+});
+
+// export app js
+module.exports = app;
--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("server running!");
+require("dotenv").config();
+const app = require("./app");
+const connectWithDb = require("./config/db");
+
+// connect with database
+connectWithDb();
+
+//connect with cloudinary
+const cloudinary = require("cloudinary");
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on ${process.env.PORT}`);
+});
